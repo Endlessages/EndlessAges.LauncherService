61bc0351a6cb706ba9b64c1b25b52c2f907c45c0
HelloKitty <andrew.blakely@ymail.com>
2017-05-24 04:26:08 -0500

    Add installer_patcher get controller method
    ContentManager.aspx?installer_patcher=ENDLESS

--- src/EndlessAges.LauncherService/Controllers/ContentManager.cs
+++ src/EndlessAges.LauncherService/Controllers/ContentManager.cs
@@ -13,6 +13,18 @@ namespace EndlessAges.LauncherService.Controllers
 	[Route("ContentManager.aspx")]
 	public class ContentManager : Controller
 	{
+		//ContentManager.aspx?installer_patcher=ENDLESS
+		[HttpGet]
+		public IActionResult Get([FromQuery]string installer_patcher)
+		{
+			//If no valid parameter was provided in the query then return an error code
+			//This should only happen if someone is spoofing
+			//422 (Unprocessable Entity)
+			if (string.IsNullOrWhiteSpace(installer_patcher))
+				return StatusCode(422);
 
+			//TODO: What exactly should the backend return? EACentral returns a broken url
+			return Content($"{@"http://game1.endlessagesonline./ENDLESSINSTALL.cab"}\n{@"ENDLESSINSTALL.cab"}");
+		}
 	}
 }
