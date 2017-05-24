5c8409fb56ed737317f1a411ad2de2956fce7b22
HelloKitty <andrew.blakely@ymail.com>
2017-05-24 04:25:40 -0500

    Add ContentManager Controller
    This controller emulates the ContentManager.aspx service of the Endless
    Ages backend.

---
+++ src/EndlessAges.LauncherService/Controllers/ContentManager.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EndlessAges.LauncherService.Controllers
+{
+	/// <summary>
+	/// Emulated controller ContentManager.aspx from the Endless Ages backend.
+	/// </summary>
+	[Route("ContentManager.aspx")]
+	public class ContentManager : Controller
+	{
+
+	}
+}
