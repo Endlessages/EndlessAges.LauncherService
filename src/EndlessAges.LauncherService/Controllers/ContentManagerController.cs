6a5482403a7025925a73cc434784c560c02577aa
HelloKitty <andrew.blakely@ymail.com>
2017-05-24 07:00:57 -0500

    Remove missing namespaces from ContentManagerController

--- src/EndlessAges.LauncherService/Controllers/ContentManagerController.cs
+++ src/EndlessAges.LauncherService/Controllers/ContentManagerController.cs
@@ -3,9 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Threading.Tasks;
-using System.Web.Http;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.WebApiCompatShim;
 
 namespace EndlessAges.LauncherService.Controllers
 {
